fda2d2143ab7fca57817dfbf4c550d96da54ed78
Hannah Young <hannah@corgibyte.com>
2022-03-04 13:23:58 -0800

    Update

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-import {Map, View} from 'ol';
+import {
+  Map,
+  View
+} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import XYZ from 'ol/source/XYZ';
+import SourceVector from 'ol/source/Vector';
+import Vector from 'ol/layer/Vector';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import * as olProj from 'ol/proj';
+import Overlay from 'ol/Overlay';
 
-new Map({
+const map = new Map({
   target: 'map',
   layers: [
     new TileLayer({
@@ -15,4 +24,46 @@ new Map({
     center: [0, 0],
     zoom: 2
   })
+});
+
+const vectorLayer = new Vector({
+  source: new SourceVector({
+    features: [
+      new Feature({
+        geometry: new Point(olProj.fromLonLat([20, 50]))
+      })
+    ]
+  })
+});
+map.addLayer(vectorLayer);
+
+var container = document.getElementById('popup');
+var content = document.getElementById('popup-content');
+var closer = document.getElementById('popup-closer');
+
+var overlay = new Overlay({
+  element: container,
+  autoPan: true,
+  autoPanAnimation: {
+    duration: 250
+  }
+});
+map.addOverlay(overlay);
+
+closer.onclick = () => {
+  overlay.setPosition(undefined);
+  closer.blur();
+  return false;
+};
+
+map.on('singleclick', (event) => {
+  if (map.hasFeatureAtPixel(event.pixel)) {
+    var coordinate = event.coordinate;
+
+    content.innerHTML = '<b>Hello world!</b><br />I am a popup.';
+    overlay.setPosition(coordinate);
+  } else {
+    overlay.setPosition(undefined);
+    closer.blur();
+  }
 });
